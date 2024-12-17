f3b70b7fc2a8ab8353369beff4df9bb551acdfb7
chshamster <162467885+chshamster@users.noreply.github.com>
2024-12-17 14:22:02 +0100

    Add files via upload

--- Fig1/ScriptGLVexp.m
+++ Fig1/ScriptGLVexp.m
@@ -1,4 +1,4 @@
-%This script generates Fig. 1a and Fig. 1b from
+%This script generates Fig. 2a and Fig. 2b from
 %Random Evolutionary Dynamics in Predator-Prey Systems Yields Large, Clustered Ecosystems
 % by Hamster, Schaap, Van Heijster and Dijksman.
 %Parameters are as described in Table 1 and the equations as outlined in
@@ -13,6 +13,7 @@ Tend = 8e4;  % Length of simulation
 % New species spawning parameters
 exist_thresh = 0.001;            % threshold below which species is considered extinct
 eta = 0.02;                      % evolutionary noise parameter when creating new species
+frac = 0.05;                      % fraction of species biomass that is transferred to daughter species
 
 NspecP = 1;   % Number of starting prey species - default 1
 NspecR = 1;   % Number of starting predator species - default 1
@@ -26,7 +27,7 @@ delta=0.4;
 Delta=zeros(expecnumspec,1); Delta(expecnumspec/2+1:end)=delta;
 beta=1/4;
 
-Tvector=0;  %vector that will store all separate t values
+Tvector=0;  %vector that will store all t values
 
 %if species j is alive, we set Alive(j)=j, otherwise 0.
 Alive=zeros(1,expecnumspec); Alive(1:NspecP)=1; 
@@ -57,27 +58,26 @@ while told<Tend && max(NtotalP,NtotalR)<expecnumspec/2
     [t,X]=ode45(@(t,x) LV(t,x,r,K,A,Delta),[told,tnew],xold,options);  %ode45 until next event
     tl=length(t);
     x(i:i+tl-2,:)=single(X(2:tl,:)); %concatenate solutions, and ignore the first value, bc its the last of the previous
-    i=i+tl-1; %update counter
+    i=i+tl-1;                        %update counter
     Tvector=cat(1,Tvector,t(2:end)); %concatenate time
-    xnew=X(end,:); %endpoint of simulation, i.e. initial condition of next step
-    xnew=xnew.*(xnew>exist_thresh); %set small species to 0
-    Alive=Alive.*(xnew>0);         %set extinct species in Alive to 0
-    Al=nonzeros(Alive)';           %get vector of alive species
-    A(:,Alive==0)=0;               %delete old species from matrix to keep it sparse
-    A(Alive==0,:)=0;               %delete old species from matrix to keep it sparse
-    j=randsample(Al,1,true,xnew(Al)); %choose random ancestor
+    xnew=X(end,:);                   %endpoint of simulation, i.e. initial condition of next step
+    xnew=xnew.*(xnew>exist_thresh);  %set small species to 0
+    Alive=Alive.*(xnew>0);           %set extinct species in Alive to 0
+    Al=nonzeros(Alive)';             %get vector of alive species
+    A(:,Alive==0)=0;                 %delete old species from matrix to keep it sparse
+    A(Alive==0,:)=0;                 %delete old species from matrix to keep it sparse
+    j=randsample(Al,1,true,xnew(Al));%choose random ancestor
     if j<expecnumspec/2+1 %update A for new species if ancestor is prey
-        xnew(NtotalP+1)=0.05*xnew(j); xnew(j)=0.95*xnew(j);
+        xnew(NtotalP+1)=frac*xnew(j); xnew(j)=(1-frac)*xnew(j);
         A(NtotalP+1,nonzeros(Alive(expecnumspec/2+1:end)))=min(A(j,nonzeros(Alive(expecnumspec/2+1:end)))+eta*randn(1,length(nonzeros(Alive(expecnumspec/2+1:end)))),0);
         A(expecnumspec/2+1:end,1:expecnumspec/2)=transpose(A(1:expecnumspec/2,expecnumspec/2+1:end))*-beta;
         r(NtotalP+1)=r(j);
         Alive(NtotalP+1)=NtotalP+1;
         NtotalP=NtotalP+1;
     else %update A for new species if ancestor is predator
-        xnew(expecnumspec/2+NtotalR+1)=0.05*xnew(j); xnew(j)=0.95*xnew(j);
+        xnew(expecnumspec/2+NtotalR+1)=frac*xnew(j); xnew(j)=(1-frac)*xnew(j);
         A(expecnumspec/2+NtotalR+1,nonzeros(Alive(1:expecnumspec/2)))=max(A(j,nonzeros(Alive(1:expecnumspec/2)))+beta*eta*randn(1,length(nonzeros(Alive(1:expecnumspec/2)))),0);
         A(1:expecnumspec/2,expecnumspec/2+1:end)=transpose(A(expecnumspec/2+1:end,1:expecnumspec/2))/(-beta);
-        r(NtotalR+1)=r(j);
         Alive(expecnumspec/2+NtotalR+1)=expecnumspec/2+NtotalR+1;
         NtotalR=NtotalR+1;
     end
